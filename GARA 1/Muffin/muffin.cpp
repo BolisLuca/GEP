e274fafec1b5287372944bf9cbe0a45495b760f5
BolisLuca <74198799+BolisLuca@users.noreply.github.com>
2020-11-09 17:00:55 +0100

    Problems Upload
    I added some of the problems of the past OIS contest so we can try to solve them together

---
+++ GARA 1/Muffin/muffin.cpp
@@ -0,0 +1,57 @@
+/*
+ * This template is valid both in C and in C++,
+ * so you can expand it with code from both languages.
+ */
+
+#include <stdio.h>
+#include <assert.h>
+#include <iostream>
+// constraints
+#define MAXN 1000000
+
+using namespace std;
+// input data
+int N, K, i;
+int T[MAXN]={1,-2, 4, -8, 16};
+int massimo,somma=0;
+int main() {
+	cout<<"N";
+	cin>>N;
+	cout<<"K";
+	cin>>K;
+
+//  uncomment the following lines if you want to read/write from files
+  /*freopen("muffin.input1.txt", "r", stdin);
+ freopen("muffin.output1.txt", "w", stdout);
+
+    assert(2 == scanf("%d %d", &N, &K));
+    for (i = 0; i < N; i++)
+        assert(1 == scanf("%d", &T[i]));*/
+
+    // insert your code here
+    
+    
+   for(int a=0;a<N-(K-1);a++){
+   	somma=0;
+   	
+   	for(int j=0;j<K;j++){
+   		somma=somma +T[a+j];
+	   }
+	   
+	   if(a=0){
+   	massimo=somma;
+	   }
+	   else{
+	   	if(massimo<somma){
+	   		massimo=somma;
+		   }
+	   }
+	   
+   }
+    
+
+  //  printf("%d\n", massimo); // change 42 with actual answer
+  cout<<massimo;
+    return 0;
+}
+
